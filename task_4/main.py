1a9934ed82838f8bdfc72169e4235308aa3919db
Лиза <lizadan2008.s4@gmail.com>
2025-11-10 14:35:36 +0300

    Update main.py

--- task_4/main.py
+++ task_4/main.py
@@ -1,1 +1,6 @@
+str=(input("введите строку: ")) # запрашиваем строку
+words = str.split()              # разбиваем строку на слова
+reversed_words = [w[::-1] for w in words]  # разворачиваем каждое слово
+result = " ".join(reversed_words) # собираем обратно в строку
+print(result)#выводи перевёрнутую строку на экран
 
