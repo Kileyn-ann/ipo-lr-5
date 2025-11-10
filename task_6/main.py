663c05f37444efd8f31effbff5a8d854b6514b71
Лиза <lizadan2008.s4@gmail.com>
2025-11-10 14:36:47 +0300

    Update main.py

--- task_6/main.py
+++ task_6/main.py
@@ -1,1 +1,14 @@
+# Открываем исходный файл для чтения
+with open("text.txt", "r", encoding="utf-8") as f:
+    text = f.read()
+# Разбиваем текст на слова
+words = text.split()
+# Преобразуем список слов в множество (оставляет только уникальные)
+unwords = set(words)
+# Записываем уникальные слова в новый файл
+with open("output.txt", "w", encoding="utf-8") as f:
+    for word in unwords:
+        f.write(word + "\n")
+
+print("Уникальные слова успешно записаны в output.txt")
 
